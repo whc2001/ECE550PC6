d67650f3f4d673545f57a8f39a8489f2866d64a3
whc2001 <ianwang0122@outlook.com>
2024-11-26 12:53:25 -0500

    fix: PS2 typematic masking

--- PS2_Interface.v
+++ PS2_Interface.v
@@ -7,7 +7,7 @@ module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, space_state, reset_sp
 
 	wire [7:0] received_data;
 	wire received_data_en;
-	reg release_armed;
+	reg actual_space_pressed, release_armed;
 
 	localparam RELEASE_PREFIX = 8'hF0;
 	localparam SPACE_SCANCODE = 8'h29;
@@ -16,33 +16,37 @@ module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, space_state, reset_sp
 	localparam PRESSED = 2'd1;
 	localparam RELEASED = 2'd2;
 
-	always @(posedge inclock)
-	begin
-		if (reset_space_state == 1'b1) begin
-			space_state <= IDLE;
-		end
-		if (resetn == 1'b0) begin
+	always @(posedge inclock) begin
+		if (!resetn) begin
+			actual_space_pressed <= 1'b0;
 			release_armed <= 1'b0;
 			space_state <= IDLE;
 		end
-		else if (received_data_en == 1'b1) begin
-			if (release_armed == 1'b1) begin
+		else if (reset_space_state) begin
+			space_state <= IDLE;
+		end
+		else if (received_data_en) begin
+			if (release_armed) begin
+				release_armed <= 1'b0;
 				if (received_data == SPACE_SCANCODE) begin
+					actual_space_pressed <= 1'b0;
 					space_state <= RELEASED;
 				end
+			end
+			else if (received_data == RELEASE_PREFIX) begin
+				release_armed <= 1'b1;
+			end
+			else if (received_data == SPACE_SCANCODE && !actual_space_pressed) begin
+				space_state <= PRESSED;
+				actual_space_pressed <= 1'b1;
 				release_armed <= 1'b0;
 			end
 			else begin
-				if (received_data == SPACE_SCANCODE) begin
-					space_state <= PRESSED;
-				end
-				else if (received_data == RELEASE_PREFIX) begin
-					release_armed <= 1'b1;
-				end
+				release_armed <= 1'b0;
 			end
 		end
 	end
-	
+
 	PS2_Controller PS2 (.CLOCK_50 			(inclock),
 						.reset 				(~resetn),
 						.PS2_CLK			(ps2_clock),
