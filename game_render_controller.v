d2e978fd9cee008557eee89199faf5df013703b9
whc2001 <ianwang0122@outlook.com>
2024-12-04 15:57:56 -0500

    fix: use non-blocking assignment

--- game_render_controller.v
+++ game_render_controller.v
@@ -178,50 +178,50 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 	always @(posedge iClock) begin
 		/** Reset Logic **/
 		if (iReset == 1'b1) begin
-			screen = SCREEN_TITLE;
-			bg_scroll = 0;
-			bird_y = (SCREEN_HEIGHT / 2) - (BIRD_HEIGHT / 2);
-			score = 0;
-			pipe_1_x = SCREEN_WIDTH * 2;
-			pipe_1_y = 0;
-			pipe_2_x = SCREEN_WIDTH * 2;
-			pipe_2_y = 0;
-			pipe_3_x = SCREEN_WIDTH * 2;
-			pipe_3_y = 0;
-			bg_cur_x = 0;
-			bird_flap_state = 0;
+			screen <= SCREEN_TITLE;
+			bg_scroll <= 0;
+			bird_y <= (SCREEN_HEIGHT / 2) - (BIRD_HEIGHT / 2);
+			score <= 0;
+			pipe_1_x <= SCREEN_WIDTH * 2;
+			pipe_1_y <= 0;
+			pipe_2_x <= SCREEN_WIDTH * 2;
+			pipe_2_y <= 0;
+			pipe_3_x <= SCREEN_WIDTH * 2;
+			pipe_3_y <= 0;
+			bg_cur_x <= 0;
+			bird_flap_state <= 0;
 		end
 		/** Write Register Logic **/
 		else begin
 			if (iWScreen == 1'b1) begin
-				screen = iScreen;
+				screen <= iScreen;
 			end
 			if (iWBGScroll == 1'b1) begin
-				bg_scroll = iBGScroll;
+				bg_scroll <= iBGScroll;
 			end
 			if (iWBirdY == 1'b1) begin
-				bird_y = iBirdY;
+				bird_y <= iBirdY;
 			end
 			if (iWScore == 1'b1) begin
-				score = iScore > 999 ? 999 : iScore;
+				score <= iScore > 999 ? 999 : iScore;
 			end
 			if (iWPipe1X == 1'b1) begin
-				pipe_1_x = iPipe1X;
+				pipe_1_x <= iPipe1X;
 			end
 			if (iWPipe1Y == 1'b1) begin
-				pipe_1_y = iPipe1Y;
+				pipe_1_y <= iPipe1Y;
 			end
 			if (iWPipe2X == 1'b1) begin
-				pipe_2_x = iPipe2X;
+				pipe_2_x <= iPipe2X;
 			end
 			if (iWPipe2Y == 1'b1) begin
-				pipe_2_y = iPipe2Y;
+				pipe_2_y <= iPipe2Y;
 			end
 			if (iWPipe3X == 1'b1) begin
-				pipe_3_x = iPipe3X;
+				pipe_3_x <= iPipe3X;
 			end
 			if (iWPipe3Y == 1'b1) begin
-				pipe_3_y = iPipe3Y;
+				pipe_3_y <= iPipe3Y;
 			end
 		end
 
@@ -248,38 +248,38 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 	/** Rendering Logic - Pixel Presenting **/
 	always @(*) begin
 		/** Background **/
-		bg_pidx_in = ((x + bg_cur_x) % BG_WIDTH) + (y * BG_WIDTH);
+		bg_pidx_in <= ((x + bg_cur_x) % BG_WIDTH) + (y * BG_WIDTH);
 
 		/** Bird **/
-		is_in_bird_area = (x >= ((SCREEN_WIDTH / 2) - (BIRD_WIDTH / 2)))
+		is_in_bird_area <= (x >= ((SCREEN_WIDTH / 2) - (BIRD_WIDTH / 2)))
 							& (x < ((SCREEN_WIDTH / 2) + (BIRD_WIDTH / 2))) 
 							& (y >= bird_y) 
 							& (y < (bird_y + BIRD_HEIGHT));
-		bird_pidx_in = is_in_bird_area ? (x - ((SCREEN_WIDTH / 2) - (BIRD_WIDTH / 2))) + ((y - bird_y) * BIRD_WIDTH) : 0;
-		bird_cidx_out = (bird_flap_state == 0) ? bird_0_cidx_out : ((bird_flap_state == 1) ? bird_1_cidx_out : bird_2_cidx_out);
+		bird_pidx_in <= is_in_bird_area ? (x - ((SCREEN_WIDTH / 2) - (BIRD_WIDTH / 2))) + ((y - bird_y) * BIRD_WIDTH) : 0;
+		bird_cidx_out <= (bird_flap_state == 0) ? bird_0_cidx_out : ((bird_flap_state == 1) ? bird_1_cidx_out : bird_2_cidx_out);
 
 		/** Score **/
-		is_in_score_digit1_area = (x >= SCORE_OFFSET_X)
+		is_in_score_digit1_area <= (x >= SCORE_OFFSET_X)
 									& (x < (SCORE_OFFSET_X + NUMBER_WIDTH))
 									& (y >= SCORE_OFFSET_Y)
 									& (y < (SCORE_OFFSET_Y + NUMBER_HEIGHT));
-		is_in_score_digit2_area = (x >= (SCORE_OFFSET_X + NUMBER_WIDTH + SCORE_MARGIN))
+		is_in_score_digit2_area <= (x >= (SCORE_OFFSET_X + NUMBER_WIDTH + SCORE_MARGIN))
 									& (x < (SCORE_OFFSET_X + NUMBER_WIDTH + SCORE_MARGIN + NUMBER_WIDTH))
 									& (y >= SCORE_OFFSET_Y)
 									& (y < (SCORE_OFFSET_Y + NUMBER_HEIGHT));
-		is_in_score_digit3_area = (x >= (SCORE_OFFSET_X + (NUMBER_WIDTH + SCORE_MARGIN) * 2))
+		is_in_score_digit3_area <= (x >= (SCORE_OFFSET_X + (NUMBER_WIDTH + SCORE_MARGIN) * 2))
 									& (x < (SCORE_OFFSET_X + (NUMBER_WIDTH + SCORE_MARGIN) * 2 + NUMBER_WIDTH))
 									& (y >= SCORE_OFFSET_Y)
 									& (y < (SCORE_OFFSET_Y + NUMBER_HEIGHT));
-		score_digit_count = score > 99 ? 3 : (score > 9 ? 2 : 1);
-		score_current_digit = is_in_score_digit1_area 
+		score_digit_count <= score > 99 ? 3 : (score > 9 ? 2 : 1);
+		score_current_digit <= is_in_score_digit1_area 
 								? score_digit_count == 3 ? score / 100 : (score_digit_count == 2 ? score / 10 : score)
 								: (is_in_score_digit2_area 
 									? score_digit_count == 3 ? (score % 100) / 10 : (score_digit_count == 2 ? score % 10 : 0)
 									: (is_in_score_digit3_area 
 										? score_digit_count == 3 ? score % 10 : 0
 										: 0));
-		number_pidx_in = is_in_score_digit1_area
+		number_pidx_in <= is_in_score_digit1_area
 							? (x - SCORE_OFFSET_X) + ((y - SCORE_OFFSET_Y) * NUMBER_WIDTH) 
 							: ((is_in_score_digit2_area & (score_digit_count >= 2))
 								? ((x - (SCORE_OFFSET_X + NUMBER_WIDTH + SCORE_MARGIN)) + ((y - SCORE_OFFSET_Y) * NUMBER_WIDTH))
@@ -288,58 +288,58 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 									: 0));
 
 		/** Pipes **/
-		pipe_1_valid = (pipe_1_x >= -PIPE_WIDTH)
+		pipe_1_valid <= (pipe_1_x >= -PIPE_WIDTH)
 							& (pipe_1_x < SCREEN_WIDTH)
 							& (pipe_1_y >= 0)
 							& (pipe_1_y < SCREEN_HEIGHT - PIPE_GAP_HEIGHT);
-		pipe_2_valid = (pipe_2_x >= -PIPE_WIDTH)
+		pipe_2_valid <= (pipe_2_x >= -PIPE_WIDTH)
 							& (pipe_2_x < SCREEN_WIDTH)
 							& (pipe_2_y >= 0)
 							& (pipe_2_y < SCREEN_HEIGHT - PIPE_GAP_HEIGHT);
-		pipe_3_valid = (pipe_3_x >= -PIPE_WIDTH)
+		pipe_3_valid <= (pipe_3_x >= -PIPE_WIDTH)
 							& (pipe_3_x < SCREEN_WIDTH)
 							& (pipe_3_y >= 0)
 							& (pipe_3_y < SCREEN_HEIGHT - PIPE_GAP_HEIGHT);
 
-		is_in_pipe_1_top_area = pipe_1_valid
+		is_in_pipe_1_top_area <= pipe_1_valid
 								& ($signed(x) >= pipe_1_x)
 								& ($signed(x) < pipe_1_x + PIPE_WIDTH)
 								& ($signed(y) >= 0)
 								& ($signed(y) < pipe_1_y);
-		is_in_pipe_2_top_area = pipe_2_valid
+		is_in_pipe_2_top_area <= pipe_2_valid
 								& ($signed(x) >= pipe_2_x)
 								& ($signed(x) < pipe_2_x + PIPE_WIDTH)
 								& ($signed(y) >= 0)
 								& ($signed(y) < pipe_2_y);
-		is_in_pipe_3_top_area = pipe_3_valid
+		is_in_pipe_3_top_area <= pipe_3_valid
 								& ($signed(x) >= pipe_3_x)
 								& ($signed(x) < pipe_3_x + PIPE_WIDTH)
 								& ($signed(y) >= 0)
 								& ($signed(y) < pipe_3_y);
-		is_in_pipe_1_bottom_area = pipe_1_valid
+		is_in_pipe_1_bottom_area <= pipe_1_valid
 								& ($signed(x) >= pipe_1_x)
 								& ($signed(x) < pipe_1_x + PIPE_WIDTH)
 								& ($signed(y) >= pipe_1_y + PIPE_GAP_HEIGHT)
 								& ($signed(y) < SCREEN_HEIGHT);
-		is_in_pipe_2_bottom_area = pipe_2_valid 
+		is_in_pipe_2_bottom_area <= pipe_2_valid 
 								& ($signed(x) >= pipe_2_x)
 								& ($signed(x) < pipe_2_x + PIPE_WIDTH)
 								& ($signed(y) >= pipe_2_y + PIPE_GAP_HEIGHT)
 								& ($signed(y) < SCREEN_HEIGHT);
-		is_in_pipe_3_bottom_area = pipe_3_valid 
+		is_in_pipe_3_bottom_area <= pipe_3_valid 
 								& ($signed(x) >= pipe_3_x)
 								& ($signed(x) < pipe_3_x + PIPE_WIDTH)
 								& ($signed(y) >= pipe_3_y + PIPE_GAP_HEIGHT)
 								& ($signed(y) < SCREEN_HEIGHT);
 
-		pipe_up_pidx_in = is_in_pipe_1_top_area
+		pipe_up_pidx_in <= is_in_pipe_1_top_area
 							? ((x - pipe_1_x) + ((y + (PIPE_HEIGHT - pipe_1_y)) * PIPE_WIDTH))
 							: (is_in_pipe_2_top_area
 								? ((x - pipe_2_x) + ((y + (PIPE_HEIGHT - pipe_2_y)) * PIPE_WIDTH))
 								: (is_in_pipe_3_top_area
 									? ((x - pipe_3_x) + ((y + (PIPE_HEIGHT - pipe_3_y)) * PIPE_WIDTH))
 									: 0));
-		pipe_down_pidx_in = is_in_pipe_1_bottom_area
+		pipe_down_pidx_in <= is_in_pipe_1_bottom_area
 							? ((x - pipe_1_x) + ((y - pipe_1_y - PIPE_GAP_HEIGHT) * PIPE_WIDTH))
 							: (is_in_pipe_2_bottom_area
 								? ((x - pipe_2_x) + ((y - pipe_2_y - PIPE_GAP_HEIGHT) * PIPE_WIDTH))
@@ -348,25 +348,25 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 									: 0));
 
 		/** Title **/
-		is_in_title_area = (x >= TITLE_OFFSET_X)
+		is_in_title_area <= (x >= TITLE_OFFSET_X)
 							& (x < (TITLE_OFFSET_X + TITLE_WIDTH))
 							& (y >= TITLE_OFFSET_Y)
 							& (y < (TITLE_OFFSET_Y + TITLE_HEIGHT));
-		title_pidx_in = is_in_title_area ? (x - TITLE_OFFSET_X) + ((y - TITLE_OFFSET_Y) * TITLE_WIDTH) : 0;
+		title_pidx_in <= is_in_title_area ? (x - TITLE_OFFSET_X) + ((y - TITLE_OFFSET_Y) * TITLE_WIDTH) : 0;
 
 		/** Play Button **/
-		is_in_play_button_area = (x >= PLAY_BUTTON_OFFSET_X)
+		is_in_play_button_area <= (x >= PLAY_BUTTON_OFFSET_X)
 								& (x < (PLAY_BUTTON_OFFSET_X + PLAY_BUTTON_WIDTH))
 								& (y >= PLAY_BUTTON_OFFSET_Y)
 								& (y < (PLAY_BUTTON_OFFSET_Y + PLAY_BUTTON_HEIGHT));
-		play_button_pidx_in = is_in_play_button_area ? (x - PLAY_BUTTON_OFFSET_X) + ((y - PLAY_BUTTON_OFFSET_Y) * PLAY_BUTTON_WIDTH) : 0;
+		play_button_pidx_in <= is_in_play_button_area ? (x - PLAY_BUTTON_OFFSET_X) + ((y - PLAY_BUTTON_OFFSET_Y) * PLAY_BUTTON_WIDTH) : 0;
 
 		/** Game Over **/
-		is_in_game_over_area = (x >= GAME_OVER_OFFSET_X)
+		is_in_game_over_area <= (x >= GAME_OVER_OFFSET_X)
 								& (x < (GAME_OVER_OFFSET_X + GAME_OVER_WIDTH))
 								& (y >= GAME_OVER_OFFSET_Y)
 								& (y < (GAME_OVER_OFFSET_Y + GAME_OVER_HEIGHT));
-		game_over_pidx_in = is_in_game_over_area ? (x - GAME_OVER_OFFSET_X) + ((y - GAME_OVER_OFFSET_Y) * GAME_OVER_WIDTH) : 0;
+		game_over_pidx_in <= is_in_game_over_area ? (x - GAME_OVER_OFFSET_X) + ((y - GAME_OVER_OFFSET_Y) * GAME_OVER_WIDTH) : 0;
 	end
 
 	/** Rendering Logic - Pixel Rendering **/
@@ -374,14 +374,14 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 		// switch by screen
 		case (screen)
 			SCREEN_TITLE: begin
-				color_cidx_in = ((is_in_title_area & (title_cidx_out != 0))
+				color_cidx_in <= ((is_in_title_area & (title_cidx_out != 0))
 					? title_cidx_out
 					: ((is_in_play_button_area & (play_button_cidx_out != 0))
 					? play_button_cidx_out
 					: bg_cidx_out));
 			end
 			SCREEN_PLAY: begin
-				color_cidx_in = (((is_in_score_digit1_area | is_in_score_digit2_area | is_in_score_digit3_area) & (number_cidx_out != 0)) 
+				color_cidx_in <= (((is_in_score_digit1_area | is_in_score_digit2_area | is_in_score_digit3_area) & (number_cidx_out != 0)) 
 					? number_cidx_out 
 					: (((is_in_pipe_1_top_area | is_in_pipe_2_top_area | is_in_pipe_3_top_area) & (pipe_up_cidx_out != 0))
 					? pipe_up_cidx_out
@@ -392,7 +392,7 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 					: bg_cidx_out))));
 			end
 			SCREEN_GAME_OVER: begin
-				color_cidx_in = ((is_in_game_over_area & (game_over_cidx_out != 0))
+				color_cidx_in <= ((is_in_game_over_area & (game_over_cidx_out != 0))
 					? game_over_cidx_out
 					: (((is_in_score_digit1_area | is_in_score_digit2_area | is_in_score_digit3_area) & (number_cidx_out != 0)) 
 					? number_cidx_out 
@@ -405,7 +405,7 @@ module game_render_controller(oPixel, iClock, iAddress, iReset,
 					: bg_cidx_out)))));
 			end
 			default: begin
-				color_cidx_in = 0;
+				color_cidx_in <= 0;
 			end
 		endcase
 	end
