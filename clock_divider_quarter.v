11e0a5bfa7f8fe40969878711bfccfbb2ba79ffe
whc2001 <ianwang0122@outlook.com>
2024-12-05 22:55:18 -0500

    fix: use behavioral clock divider

--- clock_divider_quarter.v
+++ clock_divider_quarter.v
@@ -1,12 +1,26 @@
 module clock_divider_quarter(clkout, clkin, reset);
 	input clkin, reset;
-	output clkout;
+	output reg clkout;
 	
-	wire dffe1_out, dffe1_in, dffe2_in;
-
-	not inv1(dffe1_in, dffe1_out);
-	dffe_ref dff1(dffe1_out, dffe1_in, clkin, 1'b1, reset);
+	reg [31:0] i;
 	
-	not inv2(dffe2_in, clkout);
-	dffe_ref dff2(clkout, dffe2_in, dffe1_out, 1'b1, reset);
+	localparam DIV = 4;
+	
+	always @(posedge clkin or posedge reset) begin
+		if (reset) begin
+			i <= 0;
+			clkout <= 0;
+		end
+		else begin
+			i <= (i + 1) % DIV;
+			if (i == DIV - 1) clkout <= ~clkout;
+		end
+	end
+//	wire dffe1_out, dffe1_in, dffe2_in;
+//
+//	not inv1(dffe1_in, dffe1_out);
+//	dffe_ref dff1(dffe1_out, dffe1_in, clkin, 1'b1, reset);
+//	
+//	not inv2(dffe2_in, clkout);
+//	dffe_ref dff2(clkout, dffe2_in, dffe1_out, 1'b1, reset);
 endmodule
